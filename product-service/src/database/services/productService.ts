27047404cd0ac36d17622e8efca1c5d29a531f46
Damir Fatkulin <internet.zvonok@gmail.com>
2021-09-08 03:15:54 +0600

    [rs-school] task4. TASK 4.2 get product by id query from database.

--- product-service/src/database/services/productService.ts
+++ product-service/src/database/services/productService.ts
@@ -19,3 +19,22 @@ export const getAllProducts = async (): Promise<Product[]> => {
     client.end();
   }
 };
+
+export const getProduct = async (id: string): Promise<Product[]> => {
+  const client = new Client(dbOptions);
+  const query =
+      `SELECT p.id, p.title, p.description, p.price, s.count 
+        FROM products p 
+        LEFT JOIN stocks s ON p.id = s.product_id 
+        WHERE p.id='${id}'`;
+
+  try {
+    await client.connect();
+    const {rows: result} = await client.query(query);
+    return result;
+  } catch (error) {
+    throw new Error('Failed to get product from database + ' + error);
+  } finally {
+    client.end();
+  }
+};
--- product-service/src/functions/getProductsById/handler.ts
+++ product-service/src/functions/getProductsById/handler.ts
@@ -1,27 +1,13 @@
 import 'source-map-support/register';
 import {formatJSONResponse} from '@libs/apiGateway';
 import {middyfy} from '@libs/lambda';
-import productsListJson from '../../../productList.json';
-
-export const findProduct = async (productId: string, productsListJson) => {
-  const productsList = await Promise.resolve(productsListJson);
-  let productResult;
-  productsList.forEach((element) => {
-    if (element.id == productId) {
-      productResult = element;
-    }
-  });
-  if (productResult == null) {
-    throw new Error('Product not found. Product id:' + productId);
-  }
-  return productResult;
-};
+import {getProduct} from '../../database/services/productService';
 
 export const getProductsById = async (event) => {
   try {
     const productId: string = event.pathParameters.productId;
-    let productResult = await findProduct(productId, productsListJson);
-    return formatJSONResponse(200, productResult);
+    let productResult = await getProduct(productId);
+    return formatJSONResponse(200, {product: productResult});
   } catch (err) {
     return formatJSONResponse(404, {
       message: err.message,
