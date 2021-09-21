ae0bc557b63780c5f258d5984cd51f9eb84651d0
Damir Fatkulin <internet.zvonok@gmail.com>
2021-09-21 20:59:21 +0600

    [rs-school] task5. Task 5.1 import-service.

--- import-service/src/functions/importProductsFile/handler.ts
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -3,14 +3,25 @@ import 'source-map-support/register';
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
+import * as AWS from 'aws-sdk'
 
 import schema from './schema';
 
 const importProductsFile: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  return formatJSONResponse({
-    message: `Hello ${event.queryStringParameters.name}, welcome to the exciting Serverless world!`,
-    event,
+  const s3 = new AWS.S3({
+    region: 'eu-west-1'
   });
+  const BUCKET = 'dmf-task5';
+  const catalogPath = `uploaded/${event.queryStringParameters.name}`;
+  const params = {
+    Bucket: BUCKET,
+    Key: catalogPath,
+    Expires: 60,
+    ContentType: 'text/csv'
+  };
+
+  const uploadUrl: string = await s3.getSignedUrl('putObject', params);
+  return formatJSONResponse(200, {url: uploadUrl});
 };
 
 export const main = middyfy(importProductsFile);
